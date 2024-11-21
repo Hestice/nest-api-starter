985a81f5dd6a200230be02aeffdb076f3218cda9
Hestice <custillano@gmail.com>
2024-11-21 20:55:32 +0800

    feat: added swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+
+  const config = new DocumentBuilder()
+    .setTitle('Starter API with NestJS')
+    .setDescription('Use as learning for api breakthrough')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
